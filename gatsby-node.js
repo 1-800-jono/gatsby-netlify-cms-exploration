b75fa4dd3bbdbad559939ef8c0f9d7e739e7765a
Jonathan Perez <jonathan.phz@gmail.com>
2018-08-21 18:06:04 -0300

    import createFIlePath

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
@@ -34,3 +35,16 @@ exports.createPages = ({ actions, graphql }) => {
     });
   });
 };
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}

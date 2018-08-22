922873526d7f7d368884a68c6a0243be55f9e4ef
Jonathan Perez <jonathan.phz@gmail.com>
2018-08-22 19:05:21 -0300

    style blog listing

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import { css } from 'emotion';
-import { rhythm } from '../utils/typography';
 import Layout from '../components/layout/Layout';
 
 const blog = ({ data }) => {
-  console.log(data);
   return (
     <Layout>
       <div>
         <h1>Blog List</h1>
-        <h4>{data.allMarkdownRemark.totalCount}</h4>
         {
           data.allMarkdownRemark.edges.map( ({ node }) => (
-            <div key={node.id}>
-              <h3 >
-                {node.frontmatter.title}{' '}
-                <span className={css`
-                  color: #bbb;
-                `}>
-                  — {node.frontmatter.date}
-                </span>
-              </h3>
-              <p>{node.excerpt}</p>
-            </div>
+            <a href={node.frontmatter.path} key={node.id}>
+              <div className={css` display: flex; `}>   
+                  <img src={node.frontmatter.tumbnail} alt="" className={css`
+                      max-width: 200px;
+                      max-height: 200px;
+                      margin-right: 1.5em;
+                      object-fit: contain;
+                      width: 100%;
+                  `}/>
+                  <div>
+                    <h3 className={css`margin-bottom: 0;`}>{node.frontmatter.title}</h3>
+                    <p className={css`
+                        color: #bbb;
+                        margin-bottom: 1.2em;
+                      `}>
+                      {node.frontmatter.date}
+                      </p>
+                    <p>{node.excerpt}</p>
+                  </div>
+              </div>
+            </a>
           ))
         }
       </div>
@@ -33,7 +40,7 @@ const blog = ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -41,6 +48,8 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM YYYY")
+            tumbnail
+            path
           }
           excerpt
         }

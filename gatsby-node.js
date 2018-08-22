9f0dfa5aa884bf4cf93f7e0b323f9e002bb8c1c8
Jonathan Perez <jonathan.phz@gmail.com>
2018-08-22 19:05:09 -0300

    comment out old createPages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,49 +1,85 @@
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions;
+// exports.createPages = ({ actions, graphql }) => {
+//   const { createPage } = actions;
 
-  const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`);
+//   const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`);
 
-  return graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
-            }
-          }
-        }
-      }
-    }
-  `).then( result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
+//   return graphql(`
+//     {
+//       allMarkdownRemark(
+//         sort: { order: DESC, fields: [frontmatter___date] }
+//         limit: 1000
+//       ) {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then( result => {
+//     if (result.errors) {
+//       return Promise.reject(result.errors)
+//     }
 
-    result.data.allMarkdownRemark.edges.forEach( ({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: blogPostTemplate,
-        context: {}, // additional data can be passed via context
-      });
-    });
-  });
-};
+//     result.data.allMarkdownRemark.edges.forEach( ({ node }) => {
+//       createPage({
+//         path: node.frontmatter.path,
+//         component: blogPostTemplate,
+//         context: {}, // additional data can be passed via context
+//       });
+//     });
+//   });
+// };
+
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
   if (node.internal.type === `MarkdownRemark`) {
-    const slug = createFilePath({ node, getNode, basePath: `src/pages` })
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
       node,
       name: `slug`,
       value: slug,
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    graphql(`
+      {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                path
+              }
+            }
+          }
+        }
+      }
+    `).then(result => {
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.path,
+          component: path.resolve(`./src/templates/blogTemplate.js`),
+          context: {
+            // Data passed to context is available
+            // in page queries as GraphQL variables.
+            slug: node.fields.slug,
+          },
+        })
+      })
+      resolve()
+    })
+  })
 }

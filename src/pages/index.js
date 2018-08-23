3abffcbf537706eb9cbba19e8bbc522f34e65cd2
Jonathan Perez <jonathan.phz@gmail.com>
2018-08-23 16:49:45 -0300

    add Graphql query to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Header from '../components/Header';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout/Layout';
+import Home from  '../templates/home-page';
 
-export default () => (
+export default ({data}) => (
   <Layout>
-    <Header headerText="Homepage awesome heading"/>
-    <p>
-      Star Maker is a science fiction novel by Olaf Stapledon, published in 1937. 
-      The book describes a history of life in the universe, dwarfing in scale Stapledon's 
-      previous book, Last and First Men (1930), a history of the human species over two billion years. 
-    </p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <Home {...data}/>
   </Layout>
 )
+
+export const homePageQuery = graphql`
+  query HomePage($path: String!){
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      frontmatter {
+        templateKey
+        title
+        hero {
+          heading
+          description
+        }
+      }
+    }
+  }
+`;

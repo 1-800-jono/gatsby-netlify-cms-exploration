8520d1b6e74467fb9b59559d59dcd7cbe099fb69
Jonathan Perez <jonathan.phz@gmail.com>
2018-08-23 16:49:08 -0300

    create Image Text Two Column component

---
+++ src/components/features/ImageTextTwoColumn.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { css } from 'emotion';
+
+const ImageTextTwoColumn = props => {
+  return (
+    <section>
+      <div>
+        <img src={} alt={imgAltText}/>
+      </div>
+      <div>
+        
+      </div>
+    </section>
+  );
+};
+
+export default ImageTextTwoColumn;

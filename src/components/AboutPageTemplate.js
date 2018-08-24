cdd06cede5f7788a66dc20eb1dda1642a97bbf36
Jonathan Perez <jonathan.phz@gmail.com>
2018-08-24 11:32:44 -0300

    remove props for one sec

--- src/components/AboutPageTemplate.js
+++ src/components/AboutPageTemplate.js
@@ -3,13 +3,15 @@ import Hero from '../components/features/Hero';
 import ImageTextTwoColumn from '../components/features/ImageTextTwoColumn';
 
 const AboutPageTemplate = props => {
-  const { hero, imageTextTwoColumn } = props.markdownRemark.frontmatter;
+  //const { hero, imageTextTwoColumn } = props.markdownRemark.frontmatter;
   return (
     <section>
-      <Hero {...hero }/>
-      <ImageTextTwoColumn data={imageTextTwoColumn}/>
+      hello
     </section>
   );
 };
 
 export default AboutPageTemplate;
+
+// <Hero {...hero }/>
+//       <ImageTextTwoColumn data={imageTextTwoColumn}/>

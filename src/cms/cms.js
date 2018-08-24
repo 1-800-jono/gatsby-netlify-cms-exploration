0afefc9330299b4a3d8a3de6b22ef0ec13ee7c64
Jonathan Perez <jonathan.phz@gmail.com>
2018-08-24 11:25:43 -0300

    comment out blog preview - causing an error

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,8 +1,35 @@
+import React from 'react';
 import CMS from 'netlify-cms';
-import "netlify-cms/dist/cms.css";
+//import "netlify-cms/dist/cms.css";
+
+import { renderToString } from "react-dom/server"
+import { renderStylesToString } from "emotion-server"
 
 import BlogPostPreview from './preview-templates/BlogPostPreview';
 import HomePreview from './preview-templates/HomePreview';
 
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
+// class CSSInjector extends React.Component {
+//   render() {
+//     return (
+//       <div
+//         ref={ref => {
+//           if (ref && !this.css) {
+//             this.css = renderStylesToString(renderToString(this.props.children))
+//             ref.ownerDocument.head.innerHTML += this.css
+//           }
+//         }}>
+//         {React.Children.only(this.props.children)}
+//       </div>
+//     )
+//   }
+// }
+
+
+
+// CMS.registerPreviewTemplate('blog', props => (
+//   <CSSInjector>
+//     <BlogPostPreview {...props} />
+//   </CSSInjector>
+// ));
 CMS.registerPreviewTemplate('home', HomePreview);
+//CMS.registerPreviewTemplate('blog', BlogPostPreview);

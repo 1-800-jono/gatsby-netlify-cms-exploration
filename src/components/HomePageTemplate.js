17c85d791ff69fa67c5c0653a5e10237404ba03c
Jonathan Perez <jonathan.phz@gmail.com>
2018-08-28 15:12:11 -0300

    remove gridItems

--- src/components/HomePageTemplate.js
+++ src/components/HomePageTemplate.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import Hero from '../components/features/Hero';
 import ImageTextTwoColumn from '../components/features/ImageTextTwoColumn';
-import GridItems from '../components/features/GridItems';
 
 const HomePageTemplate = props => {
-  const { hero, imageTextTwoColumn, gridItems } = props.markdownRemark.frontmatter;
+  const { hero, imageTextTwoColumn } = props.markdownRemark.frontmatter;
   return (
     <section>
       {console.log(props)}
       <Hero {...hero } />
       <ImageTextTwoColumn {...imageTextTwoColumn} />
-      <GridItems items={gridItems} />
     </section>
   );
 };
